dcdc596e11124d8d9a4b276c1696bc86bf68b05b
Miquel de Domingo <miquelddg@gmail.com>
2026-03-21 10:03:47 +0100

    fix(api): use /api/v1 for athletes routes

--- apps/api/src/modules/athletes/routes.ts
+++ apps/api/src/modules/athletes/routes.ts
@@ -1,6 +1,6 @@
 import type { ApiPath } from '$utils/types'
 
-const BASE_ATHLETES_PATH = '/athletes'
+const BASE_ATHLETES_PATH = '/v1/athletes'
 
 export const getAthletesPath = (path: ApiPath) => `${BASE_ATHLETES_PATH}${path}` as const
 export const getMeAthletesPath = (path: ApiPath) => getAthletesPath(`/me${path}` as const)
