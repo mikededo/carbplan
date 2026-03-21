4b2938dfed0d301413894bcec3e425ecc94a951b
Miquel de Domingo <miquelddg@gmail.com>
2026-03-21 10:03:47 +0100

    fix(api): ensure auth requests are logged

--- apps/api/src/modules/auth/index.ts
+++ apps/api/src/modules/auth/index.ts
@@ -2,6 +2,7 @@ import type { AuthServer } from '@carbplan/auth'
 
 import { Elysia } from 'elysia'
 
+import { createLoggerModule } from '$bootstrap/logger'
 import { apiErrorFactory } from '$modules/public/model'
 import { StatusMap } from '$utils/codes'
 
@@ -12,18 +13,23 @@ type AuthModuleOptions = {
 export const authModule = ({ auth }: AuthModuleOptions) => new Elysia({
   name: 'auth-module',
   prefix: '/v1'
-}).mount(auth.handler).macro({
-  auth: {
-    async resolve({ request: { headers }, status }) {
-      const authSession = await auth.api.getSession({ headers })
-      if (!authSession) {
-        return status(StatusMap.Unauthorized, apiErrorFactory.unauthorized())
-      }
+})
+  .use(createLoggerModule())
+  // Map all authentication routes to better auth equivalent, and also hide
+  // them from the docs, as we are using the better auth openapi spec
+  .all('/auth/*', ({ request }) => auth.handler(request), { detail: { hide: true } })
+  .macro({
+    auth: {
+      async resolve({ request: { headers }, status }) {
+        const authSession = await auth.api.getSession({ headers })
+        if (!authSession) {
+          return status(StatusMap.Unauthorized, apiErrorFactory.unauthorized())
+        }
 
-      return {
-        session: authSession.session,
-        user: authSession.user
+        return {
+          session: authSession.session,
+          user: authSession.user
+        }
       }
     }
-  }
-})
+  })
