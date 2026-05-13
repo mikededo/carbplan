6169e4c70d9e0042d9ecbdaf59c1c231bf538ec5
Miquel de Domingo <miquelddg@gmail.com>
2026-05-13 19:29:13 +0200

    fix(web): prefetch private layout data on server

--- apps/web/src/routes/(private)/(app)/+layout.ts
+++ apps/web/src/routes/(private)/(app)/+layout.ts
@@ -2,13 +2,10 @@ import type { LayoutLoad } from './$types'
 
 import { ok } from 'neverthrow'
 
-import { browser } from '$app/environment'
 import { athleteOptions } from '$lib/domain/settings/queries'
 
 export const load: LayoutLoad = async ({ parent }) => {
   const { privateServices, queryClient } = await parent()
 
-  if (browser) {
-    queryClient.prefetchQuery(athleteOptions(ok(privateServices.me)))
-  }
+  await queryClient.prefetchQuery(athleteOptions(ok(privateServices.me)))
 }
